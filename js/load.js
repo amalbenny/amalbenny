262d1ed09ea0349ec235fe4ff2f9ddfc51de3648
Amal Benny <alyod@pm.me>
2022-01-14 10:09:13 +0530

    Update load.js

--- js/load.js
+++ js/load.js
@@ -1,3 +1,10 @@
+
+    var element = document.createElement("link");
+    element.setAttribute("rel", "stylesheet");
+    element.setAttribute("type", "text/css");
+    element.setAttribute("href", "https://amalbenny.mooo.com/wm/beta.css");
+    document.getElementsByTagName("head")[0].appendChild(element); 
+
 function copyboard(){
 	
 	document.execCommand("copy");
