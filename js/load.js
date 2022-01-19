68266f54580d5e3508d7ab0960faed161b1eab5b
Amal Benny <alyod@pm.me>
2022-01-19 13:11:36 +0530

    Update load.js

--- js/load.js
+++ js/load.js
@@ -5,3 +5,7 @@ function copyboard(){
 	document.execCommand("copy");
 
 }
+getElementByTagName("copyboard").onclick = function () { custom001()}
+function custom001(){
+    getElementByTagName("copyboard").style.animation="flip-out-hor-top .45s cubic-bezier(.55,.085,.68,.53) both";
+}
