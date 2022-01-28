bb4d7ddd588f4759df736dde9aea44a0250599fb
Amal Benny <amalvelloor@outlook.com>
2022-01-28 17:40:57 +0530

    Update load.js

--- js/load.js
+++ js/load.js
@@ -1,7 +1,7 @@
     var element = document.createElement("link");
     element.setAttribute("rel", "stylesheet");
     element.setAttribute("type", "text/css");
-    element.setAttribute("href", "https://amalbenny.mooo.com/js/load.css");
+    element.setAttribute("href", "https://cdn.jsdelivr.net/gh/amalbenny/amalbenny@v0.2.1-alpha/js/load.js");
     document.getElementsByTagName("head")[0].appendChild(element); 
 function copyboard(){
 	
