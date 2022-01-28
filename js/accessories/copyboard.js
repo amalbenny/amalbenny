bfeb125b97fb33f457f5a4f7f7e673fa643945fe
Amal Benny <amalvelloor@outlook.com>
2022-01-28 17:53:50 +0530

    Create copyboard.js

---
+++ js/accessories/copyboard.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/amalbenny/amalbenny@v0.2.1-alpha/js/load.js"></script>
+<title> Copyboard tag example</title>
+</head>
+<body>
+
+<copyboard >
+	hi i am amal
+</copyboard>
+</body>
+</html>
