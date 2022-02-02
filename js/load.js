027788f4ca64637182f14c03653cd6109efd99be
Amal Benny <amalvelloor@outlook.com>
2022-02-02 08:44:18 +0530

    Update load.js

--- js/load.js
+++ js/load.js
@@ -3,13 +3,6 @@
     element.setAttribute("type", "text/css");
     element.setAttribute("href", "https://amalbenny.mooo.com/js/load.css");
     document.getElementsByTagName("head")[0].appendChild(element); 
-
-mySwitch = document.getElementsByTagName("copyboard")[0]
-mySwitch.onclick = myFunction;
-
-function myFunction() {
-  document.mySwitch[0].style.backgroundColor = "yellow";
-}
 function copyboard(){
 	
 	document.execCommand("copy");
