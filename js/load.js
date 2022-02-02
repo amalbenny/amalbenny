12232be6922e070b25f91503fbc99c2cd5245299
Amal Benny <amalvelloor@outlook.com>
2022-02-02 13:23:48 +0530

    Update load.js

--- js/load.js
+++ js/load.js
@@ -4,6 +4,22 @@
     element.setAttribute("href", "https://amalbenny.mooo.com/js/load.css");
     document.getElementsByTagName("head")[0].appendChild(element); 
 function copyboard(){
-	
-	document.execCommand("copy");
+  /* Get the text field */
+  var copyText = document.getElementById("myInput");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+   /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+  var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Copied: " + copyText.value;
+
+}
+
+function outFunc(){
+  var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Copy to clipboard";
 }
