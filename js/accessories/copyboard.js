42078c3cab87fbc5f73592e066e0ed5df336b932
Amal Benny <amalvelloor@outlook.com>
2022-02-02 13:36:40 +0530

    Update copyboard.js

--- js/accessories/copyboard.js
+++ js/accessories/copyboard.js
@@ -1,13 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/amalbenny/amalbenny@v0.2.1-alpha/js/load.js"></script>
-<title> Copyboard tag example</title>
+<title> Copyboard function example</title>
+<link rel="stylesheet" type="text/css" href="https://iamal.mooo.com/web-mark/stylesheet.css">
+<script type="text/javascript" src="https://amalbenny.mooo.com/js/load.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
 
-<copyboard >
-	hi i am amal
-</copyboard>
+<p>Click on the button to copy the text from the text field. Try to paste the text (e.g. ctrl+v) afterwards in a different window, to see the effect.</p><br><hr><p>hi </p>
+<input type="text" value="&gt; I am al visit amalbenny.mooo.com" id="myInput" disabled>
+
+<div class="hoverme">
+<button onclick="copyboard();" onmouseout="outFunc()">
+  <span class="tooltip" id="myTooltip">Copy to clipboard</span>
+  Copied text
+  </button>
+</div>
 </body>
 </html>
